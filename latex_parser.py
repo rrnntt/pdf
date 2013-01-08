61dad46d1d2371812960d0b469bd88bf4964508d
rrnntt <rrnntt@ubuntu>
2013-01-08 22:07:48 +0000

    Latex base class

--- latex_parser.py
+++ latex_parser.py
@@ -2,13 +2,24 @@
 from string_parser import *
 
 # commands without arguments
-command_names_0 = ['alpha', 'beta', 'gamma']
+text_command_names_0 = ['alpha', 'beta', 'gamma']
 
-class CommandParser(Parser):
-    """Parses a latex command of the form: \command_name ."""
-    def __init__(self):
-        """Constructor."""
+class LatexParser(Parser):
+    """Base class for latex parsers."""
+    def __init__(self, mode = 'text'):
+        """Constructor.
+    
+        Args:
+            mode (str): a latex mode: 'text' or 'math'
+        """
         Parser.__init__(self)
+        self._mode = mode
+
+class CommandParser(LatexParser):
+    """Parses a latex command of the form: \command_name ."""
+    def __init__(self, mode):
+        """Constructor."""
+        LatexParser.__init__(self, mode)
         self._nameParser = SeqParser()
         self._nameParser.addParser( CharParser('\\') )
         self._nameParser.addParser( AlphaParser() )
@@ -30,3 +41,4 @@ class CommandParser(Parser):
 
     def _simpleCommand(self, start):
         return (True, self._nameParser.getEnd() - start)
+    
